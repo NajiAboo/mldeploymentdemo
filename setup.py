28bdfc1500872a3f74e10ec65c3bf43b023ea503
USER <naji.aboo@triassicsolutions.com>
2022-06-20 08:09:42 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+from typing import List
+
+REQUIREMENT_FILE_NAME = "requirements.txt"
+
+def get_requirements_list() -> List[str]:
+    """
+    Description : This function is goign to return the list of requirements 
+
+    return : This function is doign to return the name of libraries required
+    """
+    rq_packages = []
+
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        rq_packages = requirement_file.readlines().remove("-e .")
+    return rq_packages
+
+
+PROJECT_NAME = 'housing-predictor'
+VERSION ="0.0.2"
+AUTHOR ="Mohamed Naji Aboo"
+DESCRIPTION = " This is the first FSDS batch project"
+PACKAGES = ['housing']
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description= DESCRIPTION,
+    packages= find_packages(), # PACKAGES,
+    install_requires = get_requirements_list()
+)
